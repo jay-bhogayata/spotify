80062b912e64ba63f573794559b4439c316b5fb5
JayBhogayata <jaybhogayata53@gmail.com>
2022-04-07 01:07:18 +0530

    logo

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: ["*"],
+  theme: {
+    extend: {},
+    screens: {
+      sm: '300px',
+      md: '768px',
+      lg: '976px',
+      xl: '1440px',
+    },
+  },
+  plugins: [],
+};
